9e83ee0c8b0da706d5e3fcb399754673da8ffef8
mashrur-rahman-fahim <mashrur950@gmail.com>
2025-09-02 18:35:59 +0600

    fix docker for frontend import issue

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -1,10 +1,9 @@
 import React, { useEffect, useState } from "react";
-import api from "../utils/axios";
+import api from "../utils/axios.js";
 import { useContext } from "react";
-import { VerifyContext } from "../../context/create_verify_context";
+import { VerifyContext } from "../../context/create_verify_context.jsx";
 import { useNavigate } from "react-router-dom";
 export const Post = () => {
-
   const { isVerified, verifyUser, loading } = useContext(VerifyContext);
   const navigate = useNavigate();
   const [FormData, setFormData] = useState({
@@ -28,7 +27,6 @@ export const Post = () => {
           Authorization: `Bearer ${localStorage.getItem("token")}`, // Include token in headers
         },
       });
-
 
       console.log("Post created:", response.data);
     } catch (error) {
